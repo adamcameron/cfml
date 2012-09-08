3018c41c3e6076fba3875af6a556d961ac5b69cc
Adam Cameron <adamcameroncoldfusion@gmail.com>
2012-09-08 17:52:00 +0100

    A coupla tweaks to the charts

--- blogExamples/surveys/intbug/ChartData.js
+++ blogExamples/surveys/intbug/ChartData.js
@@ -23,24 +23,24 @@ ChartData = function(chartDesign){
 			{"values" : [5], text:"No","background-color":chartDesign.seriesColours[11]}
 		],
 		q6 : [
-			{"values" : [67], text:"Yes", "background-color":chartDesign.seriesColours[0]},
-			{"values" : [3], text:"No","background-color":chartDesign.seriesColours[1]}
+			{"values" : [67], text:"Yes", "background-color":chartDesign.seriesColours[12]},
+			{"values" : [3], text:"No","background-color":chartDesign.seriesColours[13]}
 		],
 		q7 : [
-			{"values" : [4], text:"Yes", "background-color":chartDesign.seriesColours[2]},
-			{"values" : [66], text:"No","background-color":chartDesign.seriesColours[3]}
+			{"values" : [4], text:"Yes", "background-color":chartDesign.seriesColours[0]},
+			{"values" : [66], text:"No","background-color":chartDesign.seriesColours[1]}
 		],
 		q8 : [
-			{"values" : [1], text:"It's not a bug", "background-color":chartDesign.seriesColours[4]},
-			{"values" : [0], text:"It's a bug but I have code that relies on this bug, so the bug should not be fixed", "background-color":chartDesign.seriesColours[5]},
-			{"values" : [8], text:"It's a bug and if it was fixed it's fine that I would need to also fix any code I have that relies on it", "background-color":chartDesign.seriesColours[6]},
-			{"values" : [60], text:"I have no code that relies on the current behaviour, but it's a bug and it should be fixed", "background-color":chartDesign.seriesColours[7]},
-			{"values" : [1], text:"It's a bug but it should not be fixed","background-color":chartDesign.seriesColours[8]}
+			{"values" : [1], text:"It's not a bug", "background-color":chartDesign.seriesColours[2]},
+			{"values" : [0], text:"It's a bug but I have code that relies on this bug, so the bug should not be fixed", "background-color":chartDesign.seriesColours[3]},
+			{"values" : [8], text:"It's a bug and if it was fixed it's fine that I would need to also fix any code I have that relies on it", "background-color":chartDesign.seriesColours[4]},
+			{"values" : [60], text:"I have no code that relies on the current behaviour, but it's a bug and it should be fixed", "background-color":chartDesign.seriesColours[5]},
+			{"values" : [1], text:"It's a bug but it should not be fixed","background-color":chartDesign.seriesColours[6]}
 		],
 		q9 : [
-			{"values" : [58], text:"Disappointed", "background-color":chartDesign.seriesColours[9]},
-			{"values" : [8], text:"Ambivalent","background-color":chartDesign.seriesColours[10]},
-			{"values" : [4], text:"It's the correct course if action","background-color":chartDesign.seriesColours[11]}
+			{"values" : [58], text:"Disappointed", "background-color":chartDesign.seriesColours[7]},
+			{"values" : [8], text:"Ambivalent","background-color":chartDesign.seriesColours[8]},
+			{"values" : [4], text:"It's the correct course if action","background-color":chartDesign.seriesColours[9]}
 		]		
 	};
 	return this;
