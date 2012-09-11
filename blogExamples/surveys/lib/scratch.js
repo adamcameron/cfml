13d0cd4f856504b5dd8f2a5f93f7c59a8e0322d0
Adam Cameron <adamcameroncoldfusion@gmail.com>
2012-09-11 08:14:47 +0100

    Committing conflict resolutions

---
+++ blogExamples/surveys/lib/scratch.js
@@ -0,0 +1,51 @@
+{
+"show-progress":false,
+"graphset":[
+    {
+        "legend":{
+            "position":"100% 0%",
+            "margin-top":10,
+            "margin-right":10,
+            "margin-left":10,
+            "margin-bottom":10,
+            "layout":"x1",
+            "draggable":false,
+            "minimize":false
+        },
+        "plot":{
+            
+        },
+        "histogram":false,
+        "stacked":true,
+        "type":"bar",
+        "scale-x":{
+			values: ["AJAX integration","Authorisation framework (*)","CFAdmin API","CFX tags","Charting","DB drivers","Ehcache integration","Email (**)","Event gateways","File system processing","Flash/ActionScript integration","Form elements","Image processing","Monitoring","MS Exchange / SharePoint integration","ORM","Packaging / deployment","PDF processing","Presentation / reporting","Scheduling","Solr","UI wizards (***)","VFS","Web services","Websockets","XLS processing"]
+        },
+        "scale-y":{
+            
+        },
+        "series":[
+            {
+                "values":[9,12,18,12,8,39,8,34,7,40,4,12,15,12,3,18,10,12,6,28,10,2,7,21,14,8],
+                "text":"Core"
+            },
+            {
+                "values":[10,6,3,9,7,8,5,9,5,9,3,8,9,1,0,9,2,10,5,10,7,5,7,13,8,8],
+                "text":"ColdFusion Free"
+            },
+            {
+                "values":[12,12,16,12,21,3,12,7,17,2,18,13,20,15,8,11,12,18,20,7,17,14,11,12,9,17],
+                "text":"ColdFusion Standard"
+            },
+            {
+                "values":[3,7,8,5,7,0,10,0,9,2,7,1,2,14,23,5,15,2,8,3,7,4,8,1,6,4],
+                "text":"ColdFusion Enterprise"
+            },
+            {
+                "values":[20,17,9,16,11,4,19,4,16,1,22,20,8,12,20,11,15,12,15,6,13,29,21,7,17,17],
+                "text":"Unspecified / Unsure"
+            }
+        ]
+    }
+]
+}
---
+++ blogExamples/surveys/modularisation/charts.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	PieChartDesign = new PieChartDesign();
+	chartData	= new ChartData(PieChartDesign);
+	
+	path = "../zingchart_trial.trim";
+	
+	new Chart("q1", new PieChartDesign().standard, chartData.questions.q1, path, zingchart).render();
+	new Chart("q2", new PieChartDesign().standard, chartData.questions.q2, path, zingchart).render();
+	new Chart("q3", new PieChartDesign().standard, chartData.questions.q3, path, zingchart).render();
+	new Chart("q4", new PieChartDesign().standard, chartData.questions.q4, path, zingchart).render();
+	new Chart("q5", new PieChartDesign().standard, chartData.questions.q5, path, zingchart).render();
+	new Chart("q6", new PieChartDesign().standard, chartData.questions.q6, path, zingchart).render();
+	new Chart("q7", new PieChartDesign().standard, chartData.questions.q7, path, zingchart).render();
+	new Chart("q8", new PieChartDesign().longAnswers, chartData.questions.q8, path, zingchart).render();
+	new Chart("q9", new PieChartDesign().standard, chartData.questions.q9, path, zingchart).render();
+
+};
