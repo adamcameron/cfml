93a767722ab2eb9e688f1a8aaf298c0314259c98
Adam Cameron <dac.cfml@gmail.com>
2014-07-29 14:59:50 +0100

    7Li7D ruby 2

--- 7Li7W/ruby/day2/arrayMethods.rb
+++ 7Li7W/ruby/day2/arrayMethods.rb
@@ -1,81 +1,83 @@
 # arrayMethods.rb
 
+load "../day1/sep.rb"
+
 begin
 	puts "a[0] = 0"
 	puts a[0] = 0
 rescue Exception => e
 	puts e	
 end
-puts "\r\n\r\n"
+sep
 
 puts "a = []"
 puts a = []
-puts "\r\n\r\n"
+sep
 
 puts "[1].class"
 puts [1].class
-puts "\r\n\r\n"
+sep
 
 puts "[1].methods.include?('[]')"
 puts [1].methods.include?('[]')
-puts "\r\n\r\n"
+sep
 
 puts "# use [1].methods.include?(:[]) on ruby 1.9"
 puts [1].methods.include?(:[])
-puts "\r\n\r\n"
+sep
 
 
 puts "a[0] = 'zero'"
 puts a[0] = 'zero'
-puts "\r\n\r\n"
+sep
 
 puts "a[1] = 1"
 puts a[1] = 1
-puts "\r\n\r\n"
+sep
 
 puts "a[2] = ['two', 'things']"
 puts a[2] = ['two', 'things']
-puts "\r\n\r\n"
+sep
 
 puts "a"
 puts a
-puts "\r\n\r\n"
+sep
 
 puts "a = [[1, 2, 3], [10, 20, 30], [40, 50, 60]]"
 puts a = [[1, 2, 3], [10, 20, 30], [40, 50, 60]]
-puts "\r\n\r\n"
+sep
 
 puts "a[0][0]"
 puts a[0][0]
-puts "\r\n\r\n"
+sep
 
 puts "a[1][2]"
 puts a[1][2]
-puts "\r\n\r\n"
+sep
 
 puts "a = [1]"
 puts a = [1]
-puts "\r\n\r\n"
+sep
 
 puts "a.push(1)"
 puts a.push(1)
-puts "\r\n\r\n"
+sep
 
 puts "a = [1]"
 puts a = [1]
-puts "\r\n\r\n"
+sep
 
 puts "a.push(2)"
 puts a.push(2)
-puts "\r\n\r\n"
+sep
 
 puts "a.pop"
 puts a.pop
-puts "\r\n\r\n"
+sep
 
 puts "a.pop"
 puts a.pop
-puts "\r\n\r\n"
+sep
 
 puts "a"
 puts a
--- 7Li7W/ruby/day2/hashes.rb
+++ 7Li7W/ruby/day2/hashes.rb
@@ -1,26 +1,28 @@
 # hashes.rb
+load "../day1/sep.rb"
+
 
 puts "numbers = {1 => 'one', 2 => 'two'}"
 puts numbers = {1 => 'one', 2 => 'two'}
-puts "\r\n\r\n"
+sep
 
 puts "numbers[1]"
 puts numbers[1]
-puts "\r\n\r\n"
+sep
 
 puts "numbers[2]"
 puts numbers[2]
-puts "\r\n\r\n"
+sep
 
 puts "stuff = {:array => [1, 2, 3], :string => 'Hi, mom!'}"
 puts stuff = {:array => [1, 2, 3], :string => 'Hi, mom!'}
-puts "\r\n\r\n"
+sep
 
 puts "stuff[:string]"
 puts stuff[:string]
-puts "\r\n\r\n"
+sep
 
 puts "stuff[:array]"
 puts stuff[:array]
-puts "\r\n\r\n"
+sep
 
