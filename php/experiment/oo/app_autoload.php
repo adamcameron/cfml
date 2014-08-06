af84d79bac9c8df2696dbcf763b589eb1a1db062
Adam Cameron <dac.cfml@gmail.com>
2014-08-06 09:27:44 +0100

    PHP OO

--- php/experiment/oo/app_autoload.php
+++ php/experiment/oo/app_autoload.php
@@ -3,5 +3,5 @@
 spl_autoload_register(null, false);
 spl_autoload_extensions('.class.php');
 spl_autoload_register(function($class){
-	include "./classes/" .$class . ".class.php";
+	require "./classes/" .$class . ".class.php";
 });
