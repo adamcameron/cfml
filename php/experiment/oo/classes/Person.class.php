969e012aa8995c7e1072b5d40be0e555e384d172
Adam Cameron <dac.cfml@gmail.com>
2014-08-06 12:33:01 +0100

    PHP OO

--- php/experiment/oo/classes/Person.class.php
+++ php/experiment/oo/classes/Person.class.php
@@ -1,13 +1,25 @@
- <?php
+<?php
 // Person.php
 class Person {
+
 	public $firstName;
 	public $lastName;
+	private $fullName;
 
 
 	public function __construct($firstName, $lastName) {
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
+		$this->setFullName();
 	}
+
+	private function setFullName(){
+		$this->fullName = $this->firstName . " " . $this->lastName;
+	}
+
+	public function getFullname(){
+		return $this->fullName;
+	}
+
 
 }
--- php/experiment/oo/person.php
+++ php/experiment/oo/person.php
@@ -5,3 +5,16 @@ require_once("app_autoload.php");
 
 $boy = new Person("Zachary", "Cameron Lynch");
 print_r($boy);
+echo "<br>";
+
+echo "First Name: " . $boy->firstName . "<br>";
+echo "Last Name: " . $boy->lastName . "<br>";
+echo "<hr>";
+
+echo "exposed properties:<br>";
+foreach($boy as $key => $value){
+	echo "{$key} => {$value}<br>";
+}
+echo "<hr>";
+
+echo "Full Name via accessor: " . $boy->getFullName() . "<br>";
