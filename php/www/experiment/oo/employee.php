2e05111d78df959b874a71aee74305d832e42fab
Adam.Cameron <adam.cameron@hostelbookers.com>
2014-08-18 08:50:14 +0100

    PHP OO clarifying polymorphic code

--- php/www/experiment/oo/employee.php
+++ php/www/experiment/oo/employee.php
@@ -4,13 +4,15 @@
 require_once("app_autoload.php");
 
 $person = new Person("Zachary", "Cameron Lynch");
+echo "Class: " . get_class($person) . "<br>";
 echo "Name: " . $person->getFullName();
 
 echo "<hr>";
 
 
 $employee = new Employee("Donald", "Adam", "Cameron", 17);
-echo "Name: " . $employee->getFullName();
+echo "Class: " . get_class($employee) . "<br>";
+echo "Name: " . $employee->getFullName() . "<br>";
 echo "Employee ID: " . $employee->getEmployeeId();
 
 echo "<hr>";
