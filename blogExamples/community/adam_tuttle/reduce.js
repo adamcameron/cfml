983433457eb1f89a5be7d358d5d593a6f266318a
Adam Cameron <dac.cfml@gmail.com>
2014-09-03 17:24:50 +0100

    demonstrating Adam is right. Not this Adam, the other one

---
+++ blogExamples/community/adam_tuttle/reduce.js
@@ -0,0 +1,13 @@
+letters = [
+	["a", "b", "c", "d", "e"],
+	["b", "c", "d", "e"],
+	["a", "b", "c"]
+];
+
+result = letters.reduce(function(reduction, current, index){
+	if (index==0) return reduction;
+	return current.filter(function(element){
+		return reduction.indexOf(element) >= 0;
+	});
+}, letters[0]);
+console.dir(result);
---
+++ blogExamples/community/adam_tuttle/reduceWithoutClosure.js
@@ -0,0 +1,17 @@
+letters = [
+	["a", "b", "c", "d", "e"],
+	["b", "c", "d", "e"],
+	["a", "b", "c"]
+];
+
+
+function filterHandler(element){
+	return reduction.indexof(element) >= 0;
+};
+
+function reductionHandler(reduction, current, index){
+	if (index==0) return reduction;
+	return current.filter(filterHandler);
+}
+
+result = letters.reduce(reductionHandler, letters[0]);
