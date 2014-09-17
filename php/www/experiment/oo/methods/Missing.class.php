6d6112b3c5a60baf73e299d5e8874af78e7d79ca
Adam Cameron <dac.cfml@gmail.com>
2014-09-17 15:12:54 +0100

    PHP: __call() / onMissingMethod()

--- php/www/experiment/oo/methods/Missing.class.php
+++ php/www/experiment/oo/methods/Missing.class.php
@@ -1,12 +1,19 @@
 <?php
 // Missing.class.php
+require "../../../debug/dBug.php";
 
 class Missing
 {
 
     public function __call($name, $arguments)
     {
-        require "../../../debug/dBug.php";
+        echo sprintf("Function called: %s", __FUNCTION__);
+        new dBug(func_get_args());
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        echo sprintf("Function called: %s", __FUNCTION__);
         new dBug(func_get_args());
     }
 }
--- php/www/experiment/oo/methods/missing.php
+++ php/www/experiment/oo/methods/missing.php
@@ -6,3 +6,6 @@ require "Missing.class.php";
 $missing = new Missing();
 
 $missing->nonExistent("first", ["second"]);
+echo "<hr>";
+
+Missing::nonExistent("tuatahi", ["tuarua"]);
