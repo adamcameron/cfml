75d6c2aa55839ff76cccd4949ed398b9b93e4ed2
Adam Cameron <dac.cfml@gmail.com>
2014-09-17 12:34:58 +0100

    PHP: __FUNCTION__ / getFunctionCalledName()

--- php/www/experiment/constants/testFunction.php
+++ php/www/experiment/constants/testFunction.php
@@ -4,8 +4,6 @@
 function f()
 {
     echo sprintf("Using __FUNCTION__: %s() called<br>", __FUNCTION__);
-    $dbt = debug_backtrace();
-    echo sprintf("Using debug_backtrace(): line: %s; function: %s()<br>", $dbt[0]["line"], $dbt[0]["function"]);
 }
 
 f();
