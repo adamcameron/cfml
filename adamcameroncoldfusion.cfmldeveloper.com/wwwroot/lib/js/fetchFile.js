e0491af19b6d99532b3482c65d13c351deb2198f
Adam Cameron <dac.cfml+git@gmail.com>
2014-09-18 18:19:35 +0100

    updating to reflect new location of docs

--- adamcameroncoldfusion.cfmldeveloper.com/wwwroot/lib/js/fetchFile.js
+++ adamcameroncoldfusion.cfmldeveloper.com/wwwroot/lib/js/fetchFile.js
@@ -1,7 +1,7 @@
 // fetchFile.js
 
 $(document).ready(function(){
-	$.getGithubFile("daccfml", "scratch", "32d9407d39126757ddb64c6462f1d87579f629d1",
+	$.getGithubFile("daccfml", "cfscript", "1c6e74c928527ae63ae02e7b203d2b23c16f4291",
 		function(contents) {
 			var converter = new Showdown.converter();
 
