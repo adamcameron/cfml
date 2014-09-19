3ab3bfd93c4900c2d6f3d399424e51da1429da36
Adam Cameron <dac.cfml+git@gmail.com>
2014-09-19 14:16:06 +0100

    PHP: array_each()

---
+++ php/www/experiment/datatypes/array/array_filter.cfm
@@ -0,0 +1,35 @@
+<cfscript>
+	
+// array_filter.cfm
+array = [
+	{
+		1 = "a",
+		2 = "b",
+		3 = "c",
+		4 = "d"
+	},
+	{
+		5 = "e",
+		6 = "f",
+		7 = "g"
+	},
+	{
+		8 = "h",
+		9 = "i"
+	},
+	{
+		10 = "j"
+	}
+]
+
+
+function getChunk(array, key){
+	return array.filter(function(struct){
+		return struct.keyExists(key)
+	})
+}
+
+chunk = getChunk(array, 7)
+
+dump(var=chunk)
+</cfscript>
---
+++ php/www/experiment/datatypes/array/array_filter.php
@@ -0,0 +1,34 @@
+<?php
+// array_filter.php
+require "../../../debug/dBug.php";
+
+$a = [
+	[
+		1 => "a",
+		2 => "b",
+		3 => "c",
+		4 => "d",
+		5 => "e"
+	],
+	[
+		6 => "f",
+		7 => "g",
+		8 => "h",
+		9 => "i",
+		10 => "j"
+	]
+];
+
+
+function findChunk($array, $key){
+	return array_filter($array, function($subArray) use ($key){
+		return array_key_exists($key, $subArray);
+	});
+}
+
+$chunks = findChunk($a, 10);
+var_dump($chunks);
+
+
+
+new dBug($chunks);
