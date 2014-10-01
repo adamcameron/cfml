677084d3b91aa57ea0208599a09267891ec55234
Adam Cameron <dac.cfml@gmail.com>
2014-10-01 22:21:52 +0100

    PHP: loop schemes

--- php/www/experiment/loops/perf/baseline.php
+++ php/www/experiment/loops/perf/baseline.php
@@ -20,7 +20,6 @@ echo "<h3>Running $size iterations</h3>";
 $timeJob("Baseline (ascending)", function() use ($size){
 	for ($i=1; $i <= getSize($size); $i++){
 		$b = $i;
-		echo $i;
 	}
 });
 
@@ -28,21 +27,18 @@ $timeJob("Using variable for length (ascending)", function() use ($size){
 	$iterations = getSize($size);
 	for ($i=1; $i <= $iterations; $i++){
 		$b = $i;
-		echo $i;
 	}
 });
 
 $timeJob("Post decrement (descending)", function() use ($size){
 	for ($i=getSize($size); $i--;){ // (n-1)-0
 		$b = $i;
-		echo $i;
 	}
 });
 
 $timeJob("Pre decrement (descending)", function() use ($size){
 	for ($i=getSize($size)+1; --$i;){
 		$b = $i;
-		echo $i;
 	}
 });
 
@@ -50,6 +46,5 @@ $timeJob("Pre decrement with additional operation (descending)", function() use 
 	for ($i=getSize($size)+1; --$i;){
 		$b = $i;
 		$c = $i;
-		echo $i;
 	}
 });
