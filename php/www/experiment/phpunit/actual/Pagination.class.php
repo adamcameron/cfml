c1ba12f5fe0f6d8c903c9982536faf180227e40e
Adam Cameron <adam.cameron@hostelbookers.com>
2014-10-14 12:11:15 +0100

    TDD prezzo 17

--- php/www/experiment/phpunit/actual/Pagination.class.php
+++ php/www/experiment/phpunit/actual/Pagination.class.php
@@ -2,14 +2,24 @@
 
 class Pagination {
 
+    protected $extremityBuffer  = 2;
+    protected $proximityBuffer  = 3;
+
     public function filter($pages, $page){
         $showPrevious   = count($pages) && $page != 1;
         $showNext       = count($pages) && $page != count($pages);
+
+        $ellipses = [false,false];
+
+        if ($page >= $this->extremityBuffer + $this->proximityBuffer + 2){ // 2 = 1 for current page, 1 for at least one page to skip
+            $ellipses[0] = true;
+        }
+
         return [
             "pages"         => null,
             "showNext"      => $showNext,
             "showPrevious"  => $showPrevious,
-            "ellipses"      => null
+            "ellipses"      => $ellipses
         ];
     }
 }
