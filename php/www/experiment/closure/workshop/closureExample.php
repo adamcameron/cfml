ced1bbadbba5155d5da0d6efde38a214d160fd40
Adam Cameron <adam.cameron@hostelbookers.com>
2014-10-23 12:53:39 +0100

    misc

---
+++ php/www/experiment/closure/workshop/closureExample.php
@@ -0,0 +1,24 @@
+<?php
+function makeTimer($name){
+    $startTime = microtime(true);
+    return function($milestone) use ($name, $startTime){
+        $currentElapsedTime = microtime(true) - $startTime;
+        return sprintf("%s (%s): %dms\n", $name, $milestone, $currentElapsedTime * 1000);
+    };
+};
+
+$firstTimer = makeTimer("Timer 1");
+echo $firstTimer("started");
+sleep(1);
+
+$secondTimer = makeTimer("Timer 2");
+echo $secondTimer("started");
+
+echo $firstTimer("after Timer 2 started");
+
+sleep(1);
+echo $firstTimer("a coupla seconds later");
+sleep(1);
+echo $secondTimer("at the end");
+
+
--- php/www/experiment/misc/junk.php
+++ php/www/experiment/misc/junk.php
@@ -1,18 +1,19 @@
 <?php
 
-$testUri = "/properties/651/reviews/";
+function greet($name){
+    return "G'day $name";
+}
 
-$filters = [
-    ['pattern' => '/^\/properties\/\d+\/reviews/', 'handler' => ['jsonSource' => 'reviews.json', 'mock' => true]],
-    ['pattern' => '/^\/properties(?!.*reviews)/', 'handler' => ['jsonSource' => 'properties.json', 'mock' => true]],
-    ['pattern' => '/^\/cities/', 'handler' => ['jsonSource' => 'cities.json', 'mock' => false]],
-    ['pattern' => '/^\/suggestions/', 'handler' => ['jsonSource' => 'suggestions.json', 'mock' => false]],
-    ['pattern' => '/^\/currencies/', 'handler' => ['jsonSource' => 'currencies.json', 'mock' => false]],
-    ['pattern' => '/^\/login/', 'handler' => ['jsonSource' => 'login.json', 'mock' => false]]
-];
+echo GREET("World");
 
-$filterToApply = array_filter($filters, function($filter) use ($testUri){
-    return preg_match($filter["pattern"], $testUri);
-});
 
-var_dump($filterToApply);
+class thing {
+    public $foo = "moo";
+
+}
+
+
+$thing = new THING();
+
+var_dump($thing);
+
