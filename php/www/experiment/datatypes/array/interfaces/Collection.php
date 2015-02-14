fcd3fb691ccc4994eb51f6802c72e1a5cd3006b1
Adam Cameron <dac.cfml@gmail.com>
2015-02-15 11:46:02 +1300

    PHP: misc

--- php/www/experiment/datatypes/array/interfaces/Collection.php
+++ php/www/experiment/datatypes/array/interfaces/Collection.php
@@ -46,12 +46,12 @@ class Collection implements Iterator, Countable, ArrayAccess
     {
         return array_key_exists($index, $this->collection);
     }
-        
+
     public function offsetGet($index)
     {
         return $this->collection[$index];
     }
-        
+
     public function offsetSet($index, $value)
     {
         echo "offsetSet() called with [$index], [$value]<br>";
@@ -68,17 +68,19 @@ class Collection implements Iterator, Countable, ArrayAccess
     }
 }
 
-
+echo '<h3>initialise</h3>';
 $numbers = ['tahi', 'rua', 'toru', 'wha'];
 $numbersCollection = new Collection($numbers);
 printf('Collection after initialisation: %s<br>', json_encode($numbersCollection->getCollection()));
 echo '<hr>';
 
+echo '<h3>foreach()</h3>';
 foreach ($numbersCollection as $number) {
    echo "$number<br>";
 }
 echo '<hr>';
 
+echo '<h3>set</h3>';
 $numbersCollection[4] = 'rima';
 printf('Collection after setting [4]: %s<br>', json_encode($numbersCollection->getCollection()));
 
@@ -87,11 +89,13 @@ for ($i=0; $i < count($numbersCollection); $i++){
 }
 echo '<hr>';
 
+echo '<h3>append</h3>';
 $numbersCollection[] = 'ono';
 echo  $numbersCollection[5];
 printf('Collection after setting []: %s<br>', json_encode($numbersCollection->getCollection()));
 echo '<hr>';
 
+echo '<h3>array_map()</h3>';
 $uppercaseNumbers = array_map(function($number){
     return strtoupper($number);
 }, $numbersCollection);
@@ -110,13 +114,23 @@ printf('Collection (as array) used in array_map(): %s<br>', json_encode($upperca
 echo '<hr>';
 
 
+echo '<h3>array_chunk()</h3>';
 $chunked = array_chunk($collectionAsArray, 2);
 printf('Collection (as array) used in chunked(): %s<br>', json_encode($chunked));
 echo '<hr>';
 
-
+echo '<h3>iterator_apply()</h3>';
 iterator_apply($numbersCollection, function($numbersCollection){
     printf('%s ', $numbersCollection->current());
     return true;
 }, [$numbersCollection]);
 echo '<hr>';
+
+echo '<h3>unset()</h3>';
+unset($numbersCollection[2]);
+printf('Collection after unset(): %s<br>', json_encode($numbersCollection->getCollection()));
+unset($numbers[2]);
+printf('Array after unset(): %s<br>', json_encode($numbers));
+echo '<hr>';
+
+
