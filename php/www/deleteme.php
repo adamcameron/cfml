2f3a23c17c83f29b6b8f202780efa8847957ae38
Adam Cameron <adam.cameron@hostelbookers.com>
2015-03-31 08:57:53 +0100

    sync

--- php/www/deleteme.php
+++ php/www/deleteme.php
@@ -1,18 +1,9 @@
 <?php
+$transliterator = Transliterator::createFromRules(':: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;');
+$input = 'àòùìéëü';
 
-$a = md5('240610708'); // 0e462097431906509019562988736854
-$b = md5('QNKCDZO'); //0e830400451993494058024219903391
+$output = $transliterator->transliterate($input);
 
-$a = "stupid";
-$b = 0;
-
-echo "$a<br>";
-echo "$b<br>";
-echo "<br>";
-
-printf("%f<br>", $a);
-printf("%f<br>", $b);
-echo "<br>";
-
-
-var_dump($a == $b);
+echo $input;
+echo '<br>';
+echo $output;
