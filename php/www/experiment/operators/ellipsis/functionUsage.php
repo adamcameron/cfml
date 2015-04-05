f106c18b50160de5d2bec22d8b09164c9adc5ad0
Adam Cameron <dac.cfml+git@gmail.com>
2015-04-05 13:59:32 +0100

    PHP: ...

--- php/www/experiment/operators/ellipsis/functionUsage.php
+++ php/www/experiment/operators/ellipsis/functionUsage.php
@@ -1,21 +1,25 @@
 <?php
+// functionUsage.php
 
 function usingFixedArgSet($arg1, $arg2, $arg3){
+	echo "Defined arguments:\r\n";
 	var_dump(get_defined_vars());
-	echo str_repeat('=', 40). "\r\n";
+
+	echo "\r\nAll arguments:\r\n";
+	var_dump(func_get_args());
 }
 
 echo "Passing correct number\r\n";
-$args = ['tahi', 'rua', 'toru'];
+$args = ['arg1 value', 'arg2 value', 'arg3 value'];
 usingFixedArgSet(...$args);
 
 
 echo "Passing too many\r\n";
-$args = ['tahi', 'rua', 'toru', 'wha'];
+$args = ['arg1 value', 'arg2 value', 'arg3 value', 'arg4 value'];
 usingFixedArgSet(...$args);
 
 
 echo "Passing too few\r\n";
-$args = ['tahi', 'rua'];
+$args = ['arg1 value', 'arg2 value'];
 usingFixedArgSet(...$args);
 
