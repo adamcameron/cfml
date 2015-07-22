d92116d2057a4a2eda6257c315d89460421319c4
Adam Cameron <adam.cameron@hostelbookers.com>
2015-07-22 08:16:52 +0100

    shorthand op issue

---
+++ language/operators/shorthand/multiple/multiple.js
@@ -0,0 +1,9 @@
+// multiple.js
+
+a = 17
+b = 19
+c = 23
+
+a += (b += c)
+
+console.dir({a:a, b:b, c:c});
---
+++ language/operators/shorthand/multiple/multiple.py
@@ -0,0 +1,11 @@
+# multiple.py
+
+a = 17
+b = 19
+c = 23
+
+a += (b += c)
+
+print a
+print b
+print c
