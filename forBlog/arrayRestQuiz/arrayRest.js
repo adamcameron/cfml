389634570dd1aa185d4ace2a62ed74164e833739
Adam Cameron <adam.cameron@hostelbookers.com>
2015-09-14 08:36:23 +0100

    JavaScript: simple solution

--- forBlog/arrayRestQuiz/arrayRest.js
+++ forBlog/arrayRestQuiz/arrayRest.js
@@ -1,17 +1,8 @@
 // arrayRest.js
 var arrayRest = function(array){
-	return array.reduce(function(result, element, index, array){
-		var referenceToCurrentLastElement, nextElement;
-		if (result.length){
-			referenceToCurrentLastElement = result.slice(-1)[0];
-			nextElement = referenceToCurrentLastElement.slice();
-			nextElement.shift();
-		}else{
-			nextElement = array.slice();
-		}
-		result.push(nextElement);
-		return result;
-	}, []);
-};
+	return array.map(function(element,index,array){
+		return array.slice(index);
+	});
+}
 
 module.exports = arrayRest;
--- misc/junk.cfm
+++ misc/junk.cfm
@@ -1,7 +1,3 @@
 ﻿<cfscript>
-getTester = createObject("java", "me.adamcameron.miscellany.GetTester");
-writeDump(getTester);
-i = getTester.get("nope");
-
-writeDump(i);
+{}.keyExists("foo");
 </cfscript>
